f657e310dd754eadb4c4833cfb8617752662cfcc
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-09-17 22:56:30 +0400

    refactor: add mappers

--- src/main/java/dao/JdbcCurrenciesDao.java
+++ src/main/java/dao/JdbcCurrenciesDao.java
@@ -4,6 +4,7 @@ import entity.Currency;
 import exception.ConflictException;
 import exception.InternalServerErrorException;
 import exception.NotFoundException;
+import mapper.MapperCurrencyResultSet;
 import util.SqlLoader;
 
 import java.io.IOException;
@@ -71,7 +72,7 @@ public class JdbcCurrenciesDao implements CurrenciesDao {
                         "Currency with id " + id + " not found");
             }
 
-            return mapCurrency(resultSet);
+            return new MapperCurrencyResultSet().map(resultSet);
         } catch (SQLException e) {
             throw new InternalServerErrorException(
                     "Error reading currency with id: " + id);
@@ -85,7 +86,7 @@ public class JdbcCurrenciesDao implements CurrenciesDao {
             List<Currency> currencies = new ArrayList<>();
 
             while (resultSet.next()) {
-                currencies.add(mapCurrency(resultSet));
+                currencies.add(new MapperCurrencyResultSet().map(resultSet));
             }
             return currencies;
         } catch (SQLException e) {
@@ -135,19 +136,10 @@ public class JdbcCurrenciesDao implements CurrenciesDao {
                         "Currency with code " + code + " not found");
             }
 
-            return mapCurrency(resultSet);
+            return new MapperCurrencyResultSet().map(resultSet);
         } catch (SQLException e) {
             throw new InternalServerErrorException(
                     "Error reading currency by code: " + code);
         }
-    }
-
-    private Currency mapCurrency(ResultSet resultSet) throws SQLException {
-        return new Currency(
-                resultSet.getInt("id"),
-                resultSet.getString("code"),
-                resultSet.getString("name"),
-                resultSet.getString("sign")
-        );
     }
 }
--- src/main/java/dao/JdbcExchangeRatesDao.java
+++ src/main/java/dao/JdbcExchangeRatesDao.java
@@ -5,6 +5,7 @@ import entity.ExchangeRate;
 import exception.ConflictException;
 import exception.InternalServerErrorException;
 import exception.NotFoundException;
+import mapper.MapperExchangeRateResultSet;
 import util.SqlLoader;
 
 import java.io.IOException;
@@ -70,7 +71,7 @@ public class JdbcExchangeRatesDao implements ExchangeRatesDao {
                         "Exchange rate with id " + id + " not found");
             }
 
-            return mapExchangeRate(resultSet);
+            return new MapperExchangeRateResultSet().map(resultSet);
         } catch (SQLException e) {
             throw new InternalServerErrorException(
                     "Error reading exchange rate with ID: " + id);
@@ -84,7 +85,7 @@ public class JdbcExchangeRatesDao implements ExchangeRatesDao {
             List<ExchangeRate> exchangeRates = new ArrayList<>();
 
             while (resultSet.next()) {
-                exchangeRates.add(mapExchangeRate(resultSet));
+                exchangeRates.add(new MapperExchangeRateResultSet().map(resultSet));
             }
             return exchangeRates;
         } catch (SQLException e) {
@@ -135,38 +136,11 @@ public class JdbcExchangeRatesDao implements ExchangeRatesDao {
                         "Exchange rate for " + baseCurrencyCode + "/" + targetCurrencyCode + " not found");
             }
 
-            return mapExchangeRate(resultSet);
+            return new MapperExchangeRateResultSet().map(resultSet);
         } catch (SQLException e) {
             throw new InternalServerErrorException(
                     "Error reading exchange rate for " + baseCurrencyCode + "/" + targetCurrencyCode);
 
         }
-    }
-
-    private Currency mapBaseCurrency(ResultSet resultSet) throws SQLException {
-        return new Currency(
-                resultSet.getInt("base_currency_id"),
-                resultSet.getString("base_currency_code"),
-                resultSet.getString("base_currency_name"),
-                resultSet.getString("base_currency_sign")
-        );
-    }
-
-    private Currency mapTargetCurrency(ResultSet resultSet) throws SQLException {
-        return new Currency(
-                resultSet.getInt("target_currency_id"),
-                resultSet.getString("target_currency_code"),
-                resultSet.getString("target_currency_name"),
-                resultSet.getString("target_currency_sign")
-        );
-    }
-
-    private ExchangeRate mapExchangeRate(ResultSet resultSet) throws SQLException {
-        return new ExchangeRate(
-                resultSet.getInt("id"),
-                mapBaseCurrency(resultSet),
-                mapTargetCurrency(resultSet),
-                resultSet.getFloat("rate")
-        );
     }
 }
