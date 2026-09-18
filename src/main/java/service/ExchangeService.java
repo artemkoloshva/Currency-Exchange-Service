c1172e26a536d933699e710dff270fcdb20c8655
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-09-18 10:25:37 +0400

    feat: add exchange methode

--- src/main/java/service/ExchangeService.java
+++ src/main/java/service/ExchangeService.java
@@ -1,8 +1,20 @@
 package service;
 
+import dto.ExchangeRateRequest;
+import dto.ExchangeRateResponse;
 import dto.ExchangeRequest;
 import dto.ExchangeResponse;
 
+import java.util.List;
+
 public interface ExchangeService {
+    List<ExchangeRateResponse> getAllExchangeRates();
+
+    ExchangeRateResponse getExchangeRateByCodes(String baseCurrencyCode, String targetCurrencyCode);
+
+    ExchangeRateResponse addExchangeRate(ExchangeRateRequest request);
+
+    ExchangeRateResponse updateExchangeRate(ExchangeRateRequest request);
+
     ExchangeResponse exchange(ExchangeRequest request);
 }
