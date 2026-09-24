0b836e0d6c84ac3730e5aabb48c12174056a4faa
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-09-24 16:51:54 +0400

    refactor: change exception to DatabaseException and make class to final

--- src/main/java/util/SqlLoader.java
+++ src/main/java/util/SqlLoader.java
@@ -1,23 +1,28 @@
 package util;
 
+import exception.DatabaseException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-public class SqlLoader {
+public final class SqlLoader {
     private static final String SQL_DIR = "sql/";
+
+    private SqlLoader() {}
 
     public static String loadSqlQuery(String fileName) {
         String path = SQL_DIR + fileName;
 
         try (InputStream in = SqlLoader.class.getClassLoader().getResourceAsStream(path)) {
             if (in == null) {
-                throw new RuntimeException("SQL file not found in classpath: " + path);
+                throw new DatabaseException(
+                        "SQL file not found in classpath: " + path);
             }
-
             return new String(in.readAllBytes(), StandardCharsets.UTF_8);
         } catch (IOException e) {
-            throw new RuntimeException("Error loading SQL query from file: " + fileName, e);
+            throw new DatabaseException(
+                    "Error loading SQL query from file: " + fileName);
         }
     }
 }
